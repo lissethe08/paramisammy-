c351ed88be4de92ff7a2dc5fe55efc678f7d6575
lisi <Estefanialopezcevallos@gmail.com>
2026-01-06 00:18:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function openEnvelope() {
   document.querySelector('.envelope').classList.toggle('open');
 }
+
+/* FLORES Y CORAZONES */
+const emojis = ['❤️', '🌸', '🌷', '💐', '💖'];
+
+function createFloat() {
+  const item = document.createElement('div');
+  item.classList.add('float');
+  item.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+
+  item.style.left = Math.random() * 100 + 'vw';
+  item.style.animationDuration = (Math.random() * 3 + 5) + 's';
+
+  document.body.appendChild(item);
+
+  setTimeout(() => {
+    item.remove();
+  }, 8000);
+}
+
+setInterval(createFloat, 450);
